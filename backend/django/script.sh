98da6e261bb7e5492c99c3978baab57b8485fa35
Lyyrst <kurt.butorp@gmail.com>
2024-10-21 18:35:20 +0200

    i found the fix !

--- backend/django/script.sh
+++ backend/django/script.sh
@@ -3,7 +3,7 @@
 while ! bash -c "PGPASSWORD='$DB_PASS' psql -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME <<< '\q'" > /dev/null 2> /dev/null; do
 	echo "Waiting for PostgreSQL...";
 	sleep 1;
-done;
+done
 
 python manage.py makemigrations
 python manage.py migrate
